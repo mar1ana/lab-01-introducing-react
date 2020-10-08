da0b63daf079efa9db46c82ffc2e097dd8ef3b4f
Mariana <200398944@student.georgianc.on.ca>
2020-10-08 11:06:58 -0400

    deleted unecessary files

---
+++ src/component/Greeting.jsx
@@ -0,0 +1,13 @@
+// 1) Import React
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+// 2) Define our component function
+const Greeting = () => {
+  // 4) Must return
+  return null;
+};
+
+// 3) Export our component
+export default Greeting;
